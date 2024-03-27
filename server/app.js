1e39cb1875e9ac1cf735e0eeb2252f8f450eee06
Nwaigba66 <your.email@glorianwaigba@gmail.com>
2024-03-27 07:54:54 +0100

    done

--- server/app.js
+++ server/app.js
@@ -3,13 +3,15 @@ const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
 const PORT = process.env.PORT || 5005;
 const cors = require("cors");
-
+const mongoose = require("mongoose")
 
 // STATIC DATA
 // Devs Team - Import the provided files with JSON data of students and cohorts here:
 // ...
 const cohort= require("./cohorts.json")
 const students = require("./students.json")
+
+
 
 
 // INITIALIZE EXPRESS APP - https://expressjs.com/en/4x/api.html#express
@@ -47,3 +49,7 @@ app.get('/api/students', (req, res)=> {
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
+mongoose
+.connect("mongodb://127.0.0.1:27017/mongoose-example-dev")
+.then(x => console.log(`Connected to Database: "${x.connections[0].name}"`))
+.catch(err => console.error("Error connecting to MongoDB", err));
